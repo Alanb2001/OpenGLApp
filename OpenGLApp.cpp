9707bed9682b277c2a3070073dcfd2ec11af8121
Alanb2001 <alanb2002@gmail.com>
2025-01-13 18:17:01 +0000

    Linked glad and glfw, made a window context to see if both where linked properly

--- OpenGLApp.cpp
+++ OpenGLApp.cpp
@@ -1,6 +1,49 @@
+#include <glad.h>
+#include <glfw3.h>
+#include <iostream>
+#include <ostream>
 
-int main(int argc, char* argv[])
+static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+int main()
 {
-    
+    // Tell glfw what version of OpenGL we are using as well as what profile we want to use
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create window data with glfw
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGLApp", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << '\n';
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Let glad load the address of OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << '\n';
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, const int width, const int height)
+{
+    glViewport(0, 0, width, height);
 }
